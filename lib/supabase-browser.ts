db9909e445ff611548a6727a4e2c23359d55b674
scriptumin <scriptum.in@gmail.com>
2025-08-30 08:46:52 +0530

    Create supabase-browser.ts

---
+++ lib/supabase-browser.ts
@@ -0,0 +1,8 @@
+
+import { createClient } from "@supabase/supabase-js";
+
+export function createBrowserClient() {
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+return createClient(url, anon, { auth: { persistSession: true } });
+}
