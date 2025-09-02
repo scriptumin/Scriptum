faf697686a2a9f3160f28ebebc63e835b91f21ea
scriptumin <scriptum.in@gmail.com>
2025-09-02 06:32:25 +0530

    Create supabase.ts

---
+++ app/lib/supabase.ts
@@ -0,0 +1,8 @@
+
+import { createClient } from "@supabase/supabase-js";
+
+// Get keys from environment variables
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
